281c6bbb541b1562f4d0cb1e805afac1fa91f40b
Maciej Żok <mzok@intercars.eu>
2023-08-26 21:16:44 +0200

    Zgadywanka

--- src/Zgadywanka.java
+++ src/Zgadywanka.java
@@ -14,7 +14,6 @@ public class Zgadywanka extends JFrame implements ActionListener {
     public Zgadywanka(){
 
         JLabel lInfo, lZakres, lLiczbaProb, lSprawdz;
-        JTextField tZakres, tLiczbaProb, tSprawdz ;
         JButton bSprawdz, bZatwierdz;
 
 
@@ -56,19 +55,21 @@ public class Zgadywanka extends JFrame implements ActionListener {
         zgadywanka.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         zgadywanka.setVisible(true);
 
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    zakres = Integer.parseInt(tZakres.getText());
+    liczbaProb = Integer.parseInt(tLiczbaProb.getText());
+
         Scanner scanner = new Scanner(System.in);
         boolean poprawnaLiczba = false;
         int zmienna;
-        System.out.println("Podaj liczbę definiującą koniec zakresu losowanych liczb");
-        int zakres = scanner.nextInt();
+
         Random random = new Random();
         int liczba = random.nextInt(zakres);
-
-
-        System.out.println("Podaj ile prób chcesz maksymalnie wykonać");
-        int liczbaProb = scanner.nextInt();
-
-
+        
         for (int i = 0; i < liczbaProb; i++) {
             try {
                 if (i==0)
@@ -89,19 +90,13 @@ public class Zgadywanka extends JFrame implements ActionListener {
                 } else {
                     System.out.println("Twoja liczba " + zmienna + " ma za wysoką wartość");
                 }
-            } catch (InputMismatchException e) {
+            } catch (InputMismatchException f) {
                 System.out.println("Błąd: Niepoprawny format danych. Wprowadź poprawną liczbę.");
                 scanner.nextLine(); // Konsumujemy błąd wejścia, aby uniknąć nieskończonej pętli
             }
         }
         if (!poprawnaLiczba) {
-            System.out.println("Niestety, nie udało się odgadnąć liczby w ustalonych 100 próbach. Wylosowana liczba to: " + liczba);
+            System.out.println("Niestety, nie udało się odgadnąć liczby w ustalonych: " + liczbaProb + " próbach. Wylosowana liczba to: " + liczba);
         }
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-    zakres = Integer.parseInt(tZakres.getText());
-    liczbaProb = Integer.parseInt(tLiczbaProb.getText());
     }
 }
